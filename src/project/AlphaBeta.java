89c52c6cf5f544f2456d950bfcef682c249c882f
HarrisonNguyen92 <harrison.nguyen92@yahoo.com>
2014-06-02 20:39:45 -0700

    Added Eval(state s) it is a working ai

--- src/project/AlphaBeta.java
+++ src/project/AlphaBeta.java
@@ -59,10 +59,7 @@ public class AlphaBeta {
 					}
 					state.undo(i, j); // undo move
 				}
-				// The only problem that i have with the above statement is that
-				// it will take the FIRST best solution. If we can keep a record
-				// of all best solutions and then make a random choice, it may
-				// be better.
+				// Potential random choice area. need list and PRNG
 			}
 		}
 		System.out.println(best);
@@ -70,26 +67,19 @@ public class AlphaBeta {
 	}
 
 	private int maxValue(State state) {
-		// if (state.checkWin() != 0) return state.checkWin(); i dont think
-		// returning a simple 1 / -1 will suffice when comparing to other
-		// evaluation returns
 		if (cutoff() || depth <= 0 || state.spaces <= 0
 				|| state.checkWin() != 0)
-			return evaluate(state);
+			return eval(state);
 
-		int best = alpha;// best = -20000
+		int best = alpha;
 		depth--;
 		for (int i = 0; i < state.board.length; i++) {
 			for (int j = 0; j < state.board.length; j++) {
 				if (state.board[i][j] == 0) {
-					state.move(i, j, true);// make move
+					state.move(i, j, true);
 					best = Integer.max(best, minValue(state));
-					// the line above is basically
-					// score = max(state, st, best);
-					// if (score < best)
-					// best = score;
 					state.undo(i, j);
-					if (best > beta)// pruning
+					if (best > beta)
 						return beta;
 				}
 			}
@@ -98,31 +88,36 @@ public class AlphaBeta {
 	}
 
 	private int minValue(State state) {
-		// if (state.checkWin() != 0) return state.checkWin(); i dont think
-		// returning a simple 1 / -1 will suffice when comparing to other
-		// evaluation returns
 		if (cutoff() || depth <= 0 || state.spaces <= 0
 				|| state.checkWin() != 0)
-			return evaluate(state);
 
-		int best = beta; // best = 20000
+			return eval(state);
+		int best = beta;
 		depth--;
 		for (int i = 0; i < state.board.length; i++) {
 			for (int j = 0; j < state.board.length; j++) {
 				if (state.board[i][j] == 0) {
-					state.move(i, j, false);// make move
+					state.move(i, j, false);
 					best = Integer.min(best, maxValue(state));
-					// the line above is basically
-					// score = min(state, st, best);
-					// if (score > best)
-					// best = score;
 					state.undo(i, j);
-					if (best < alpha)// pruning
+					if (best < alpha)
 						return alpha;
 				}
 			}
 		}
 		return best;
+	}
+
+	/**
+	 * Program will return the best solution found so far given a specific
+	 * period of time
+	 * 
+	 * @return
+	 */
+	private boolean cutoff() {
+		if (System.currentTimeMillis() - startTime > limit)
+			return true;
+		return false;
 	}
 
 	/**
@@ -136,78 +131,6 @@ public class AlphaBeta {
 		for (int i = 0; i < s.board.length; i++)
 			for (int j = 0; j < s.board.length; j++)
 				score += eval(s, i, j);
-
-		// an example eval f(n) for tic tac toe was
-		// (#3lengths open for me) - (#3lengths open for you)
-		// if (s.checkWin() == 1)
-		// return Integer.MAX_VALUE / 4;
-		// if (s.checkWin() == -1)
-		// return Integer.MIN_VALUE / 4;
-		// if (s.spaces == 0)
-		// return 0;
-		// int score = 0;
-		// int xCount = 0; // variable to keep track of Xs in a row
-		// int oCount = 0; // variable to keep track of Os in a row
-		// // checks for all vertical rows possible
-		// for (int i = 0; i < s.board.length; ++i)
-		// for (int j = 0; j < s.board.length - 3; ++j) {
-		// for (int k = j; k < j + 4; ++k) {
-		// if (s.board[i][k] == 1)
-		// xCount++;
-		// if (s.board[i][k] == -1)
-		// oCount++;
-		// }
-		// if (!(xCount > 0 && oCount > 0)) {
-		// if (xCount > 0)
-		// score += xCount;
-		// if (oCount > 0)
-		// score -= oCount;
-		// xCount = 0;
-		// oCount = 0;
-		// }
-		// }
-		// xCount = 0;
-		// oCount = 0;
-		// // checks for all horizontal rows possible
-		// for (int i = 0; i < s.board.length - 3; ++i)
-		// for (int j = 0; j < s.board.length; ++j) {
-		// for (int k = i; k < i + 4; ++k) {
-		// if (s.board[k][j] == 1)
-		// xCount++;
-		// if (s.board[k][j] == -1)
-		// oCount++;
-		// }
-		// if (!(xCount > 0 && oCount > 0)) {
-		// if (xCount > 0)
-		// score += xCount;
-		// if (oCount > 0)
-		// score -= oCount;
-		// xCount = 0;
-		// oCount = 0;
-		// }
-		// }
-
-		/*
-		 * for (int i = 0; i < s.board.length; ++i) { for (int j = 0; j <
-		 * s.board.length; ++j) { if (s.board[i][j] == -1) { oCount++; score +=
-		 * xCount - 1; xCount = 0; } if (s.board[i][j] == 1) { if (i < 2 || j <
-		 * 2 || i > s.board.length - 3 || j > s.board.length - 3) xCount -= 2;
-		 * // screw the walls xCount++; score -= oCount - 1; oCount = 0; } if
-		 * (s.board[i][j] == 0) { if (j != 0) if (s.board[i][j - 1] == -1)
-		 * oCount += 1; if (j != s.board.length - 1) if (s.board[i][j + 1] ==
-		 * -1) oCount += 1; if (oCount > 2) oCount *= 2; if (oCount > 1 ||
-		 * xCount > 1) score += xCount - oCount; oCount = 0; xCount = 0; } }
-		 * xCount = 0; oCount = 0; }
-		 * 
-		 * for (int j = 0; j < s.board.length; ++j) { for (int i = 0; i <
-		 * s.board.length; ++i) { if (s.board[i][j] == -1) { oCount++; score +=
-		 * xCount - 1; xCount = 0; } if (s.board[i][j] == 1) { xCount++; score
-		 * -= oCount - 1; oCount = 0; } if (s.board[i][j] == 0) { if (i != 0) if
-		 * (s.board[i - 1][j] == -1) oCount += 1; if (i != s.board.length - 1)
-		 * if (s.board[i + 1][j] == -1) oCount += 1; if (oCount > 2) oCount *=
-		 * 2; if (oCount > 1 || xCount > 1) score += xCount - oCount; oCount =
-		 * 0; xCount = 0; } } xCount = 0; oCount = 0; }
-		 */
 		return score;
 	}
 
@@ -277,15 +200,46 @@ public class AlphaBeta {
 		return score * check;// negates if opponent
 	}
 
-	/**
-	 * Program will return the best solution found so far given a specific
-	 * period of time
-	 * 
-	 * @return
-	 */
-	private boolean cutoff() {
-		if (System.currentTimeMillis() - startTime > limit)
-			return true;
-		return false;
+	private int eval(State s) {
+		if (s.checkWin() == 1)
+			return Integer.MAX_VALUE / 2;
+		if (s.checkWin() == -1)
+			return Integer.MIN_VALUE / 2;
+		if (s.spaces == 0)
+			return 0;
+		int score = 0;
+		int x = 0;
+		int o = 0;
+
+		// checks for all vertical rows possible
+		for (int i = 0; i < s.board.length; ++i)
+			for (int j = 0; j < s.board.length - 3; ++j) {
+				for (int k = 0; k < 4; ++k) {
+					if (s.board[i][j + k] == 1)
+						x++;
+					if (s.board[i][j + k] == -1)
+						o++;
+				}
+				score += x * x;
+				score -= o * o;
+				x = 0;
+				o = 0;
+			}
+
+		// checks for all horizontal rows possible
+		for (int i = 0; i < s.board.length - 3; ++i)
+			for (int j = 0; j < s.board.length; ++j) {
+				for (int k = 0; k < 4; ++k) {
+					if (s.board[i + k][j] == 1)
+						x++;
+					if (s.board[i + k][j] == -1)
+						o++;
+				}
+				score += x * x;
+				score -= o * o;
+				x = 0;
+				o = 0;
+			}
+		return score;
 	}
 }
